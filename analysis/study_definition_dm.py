7946fb390eb7f09d77f8f15b72025a5a46005636
Ruth Costello <ruth.costello@lshtm.ac.uk>
2023-05-02 11:28:14 +0100

    study definition update

--- analysis/study_definition_dm.py
+++ analysis/study_definition_dm.py
@@ -42,56 +42,63 @@ study = StudyDefinition(
             "2020-02-01",
             returning="household_size",
         ),
-        age=patients.age_as_of(
-            "index_date",
-            return_expectations={
-                "rate": "universal",
-                "int": {"distribution": "population_ages"},
-            },
-        ),
-        # Sex
-        sex=patients.sex(
-            return_expectations={
-                "rate": "universal",
-                "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
-            },
-        ),
-        has_msoa=patients.satisfying(
-        "NOT (msoa = '')",
-            msoa=patients.address_as_of(
-            "index_date",
-            returning="msoa",
-        ),
-        return_expectations={"incidence": 1.0}
-        ),
-        imd=patients.categorised_as(
-            {
-            "0": "DEFAULT",
-            "1": """index_of_multiple_deprivation >=0 AND index_of_multiple_deprivation < 32844*1/5 AND has_msoa""",
-            "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
-            "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
-            "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
-            "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation <= 32844""",
-            },
-        index_of_multiple_deprivation=patients.address_as_of(
-            "index_date",
-            returning="index_of_multiple_deprivation",
-            round_to_nearest=100,
-            ),
+    ),
+    age=patients.age_as_of(
+    "index_date",
         return_expectations={
             "rate": "universal",
-            "category": {
-                "ratios": {
-                    "0": 0.05,
-                    "1": 0.19,
-                    "2": 0.19,
-                    "3": 0.19,
-                    "4": 0.19,
-                    "5": 0.19,
-                    }
-                },
-            },
+            "int": {"distribution": "population_ages"},
+        },
+    ),
+    # Sex
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.5, "U": 0.01}},
+        },
+    ),
+    has_msoa=patients.satisfying(
+    "NOT (msoa = '')",
+        msoa=patients.address_as_of(
+        "index_date",
+        returning="msoa",
+    ),
+    return_expectations={"incidence": 1.0}
+    ),
+    imd=patients.categorised_as(
+        {
+        "0": "DEFAULT",
+        "1": """index_of_multiple_deprivation >=0 AND index_of_multiple_deprivation < 32844*1/5 AND has_msoa""",
+        "2": """index_of_multiple_deprivation >= 32844*1/5 AND index_of_multiple_deprivation < 32844*2/5""",
+        "3": """index_of_multiple_deprivation >= 32844*2/5 AND index_of_multiple_deprivation < 32844*3/5""",
+        "4": """index_of_multiple_deprivation >= 32844*3/5 AND index_of_multiple_deprivation < 32844*4/5""",
+        "5": """index_of_multiple_deprivation >= 32844*4/5 AND index_of_multiple_deprivation <= 32844""",
+        },
+    index_of_multiple_deprivation=patients.address_as_of(
+        "index_date",
+        returning="index_of_multiple_deprivation",
+        round_to_nearest=100,
         ),
+    return_expectations={
+        "rate": "universal",
+        "category": {
+            "ratios": {
+                "0": 0.05,
+                "1": 0.19,
+                "2": 0.19,
+                "3": 0.19,
+                "4": 0.19,
+                "5": 0.19,
+                }
+            },
+        },
+    ),
+    # Migration status
+    migration_status=patients.with_these_clinical_events(
+        migration_codes,
+        on_or_before="index_date",
+        returning="binary_flag",
+        return_expectations={"incidence":0.2,},
     ),
     # Subgroups
     has_t1_diabetes=patients.with_these_clinical_events(

d10bbad1f932cf47e930a8337e8a30b0c11998b8
Ali Henderson <alasdair.henderson1@lshtm.ac.uk>
2023-06-09 15:54:35 +0100

    Update 104_poisson.R

--- analysis/104_poisson.R
+++ analysis/104_poisson.R
@@ -12,11 +12,13 @@ library(tidyr)
 library(stringr)
 library(ggplot2)
 library(patchwork)
+library(lubridate)
 bkg_colour <- "gray99"
-filepath <- "C:/Users/RuthCostello/Documents/GitHub/covid_collateral_imd/output/cvd"
+
+here::here()
 
 # import data -------------------------------------------------------------
-all_files <- list.files(filepath, pattern = "an_")
+all_files <- list.files(here::here("output/cvd"), pattern = "an_")
 outcomes <- stringr::str_remove_all(all_files, c("an_|.csv"))
 outcome_of_interest_namematch <- bind_cols("outcome" = outcomes,
                                            "outcome_name" = (c("Heart Failure", "Myocardial Infarction", "Stroke", "Venous Thromboembolism")) # nolint
@@ -24,13 +26,16 @@ outcome_of_interest_namematch <- bind_cols("outcome" = outcomes,
 
 plot_order <- c(1, 2, 3, 4)
 for(ii in 1:length(outcomes)){
-  load_file <- read.csv(paste0(filepath, "/", paste0("an_", outcomes[ii], ".csv"))) # nolint
+  load_file <- read.csv(paste0(here::here("output/cvd/"), paste0("an_", outcomes[ii], ".csv"))) # nolint
   assign(outcomes[ii], load_file)
 }
 
 # function to generate data for table 3 one outcome at a time # nolint
 tab3_function <- function(outcome){
   df_outcome <- get(outcome)
+  # convert dateA variable to R accepted format
+  df_outcome$dateA <- lubridate::as_date(df_outcome$dateA, format = "%d/%B/%y")
+  
   start_lockdown = as.Date("2020-03-23")
   end_post_lockdown_period = as.Date("2022-12-31")
   # Taken out code for creating variables as I have done this in Stata
@@ -62,11 +67,12 @@ tab3_function <- function(outcome){
   outcome_pred_nointervention <- outcome_pred %>%
     mutate_at("postcovid", ~(.=0)) 
   predicted_vals_nointervention <- predict(po_model2, newdata = outcome_pred_nointervention, se.fit = TRUE, dispersion = deviance_adjustment) 
-  stbp_noLdn <- predicted_vals_nointervention$se.fit	
   predicted_vals_noLdn <- predicted_vals_nointervention$fit	
+  stbp_noLdn <- predicted_vals_nointervention$se.fit	
   
   ## standard errors
-  df_se <- bind_cols(stbp = stbp, 
+  df_se <- bind_cols(imd = as.character(df_outcome$imd), 
+                     stbp = stbp,
                      pred = predicted_vals, 
                      stbp_noLdn = stbp_noLdn, 
                      pred_noLdn = predicted_vals_noLdn, 
@@ -77,89 +83,55 @@ tab3_function <- function(outcome){
       low = pred - (1.96*stbp),
       upp_noLdn = pred_noLdn + (1.96*stbp_noLdn),
       low_noLdn = pred_noLdn - (1.96*stbp_noLdn),
-      # probline
-      predicted_vals = exp(pred)/denom,
-      probline_noLdn = exp(pred_noLdn)/denom,
-      #
-      uci = exp(upp)/denom,
-      lci = exp(low)/denom,
-      uci_noLdn = exp(upp_noLdn)/denom,
-      lci_noLdn = exp(low_noLdn)/denom
     )
   sigdig <- 2
   model_out <- signif(ci.exp(po_model2)[2,], sigdig)
   
-  tab3_dates <- bind_cols("weekPlot" = df_outcome$date, df_se) %>%
-            drop_na() %>% 
-           # estimated number of weekly ooutcomes with NO LOCKDOWN
-           mutate(col1 = paste0(prettyNum(probline_noLdn*1e6,big.mark=",",digits = 0, scientific=FALSE), 
-                         " (", prettyNum(lci_noLdn*1e6,big.mark=",",digits = 0, scientific=FALSE), 
-                         " - ", prettyNum(uci_noLdn*1e6,big.mark=",",digits = 0, scientific=FALSE),")",
-           # estimated number of weekly ooutcomes with LOCKDOWN
-           col3 = paste0(prettyNum(predicted_vals*1e6,big.mark=",",digits = 0, scientific=FALSE), 
-                         " (", prettyNum(lci*1e6,big.mark=",",digits = 0, scientific=FALSE), 
-                         " - ", prettyNum(uci*1e6,big.mark=",",digits = 0, scientific=FALSE),")"))
-    ) %>%
-    ## filter to post-lockdown data only
-    filter(weekPlot >= start_lockdown) %>%
+  # bind predictions with dates and set a pre/post-lockdwon variable
+  tab3_merge <- bind_cols("weekPlot" = df_outcome$date, 
+                          "postcovid" = df_outcome$postcovid,
+                          df_se)
+  
+  # how many post-lockdown months of data are there? 
+  months_post <- (table(tab3_merge$postcovid)/5)[2]
+  months_pre <- (table(tab3_merge$postcovid)/5)[1]
+  # create a variable to filter all of the post-lockdown and the relevant period pre lockdown
+  tab3_merge$select <- 1
+  tab3_merge$select[1:(months_pre-months_post)] <- 0
+  
+  tab3_cumsum <- tab3_merge %>% 
+    filter(select == 1) %>% 
+    ungroup() %>% 
+    group_by(postcovid, imd) %>% 
     ## calculate cumulative sum of predicted vals with/without lockdown 
-    mutate(cumsum_ldn = cumsum(predicted_vals*1e6),
-           lci_cumsum_ldn = cumsum(lci*1e6),
-           uci_cumsum_ldn = cumsum(uci*1e6),
-           cumsum_noLdn = cumsum(probline_noLdn*1e6),
-           lci_cumsum_noLdn = cumsum(low_noLdn*1e6),
-           uci_cumsum_noLdn = cumsum(upp_noLdn*1e6),
-           prettyNum(uci*1e6,big.mark=",",digits = 0, scientific=FALSE),
-           ## weekly difference in Lockdown vs No lockdown
-           col5 = prettyNum(signif((probline_noLdn*1e6) - (predicted_vals*1e6),3), big.mark=",", digits = 0, scientific=FALSE),
-           ## cumulative sum of Lockdown vs No lockdown
-           col6 = prettyNum(signif((cumsum_noLdn) - (cumsum_ldn),3), big.mark=",", digits = 0, scientific=FALSE)
-    )  %>%
-    ## censor data if it is too small
-    mutate(diff_predicted = (probline_noLdn*1e6) - (predicted_vals*1e6),
-           cumsum_diff_predicted = (cumsum_noLdn) - (cumsum_ldn)) %>%
-    mutate_at(.vars = c("col5"), ~ifelse(diff_predicted < 10 & diff_predicted > 0,
-                                         "<10", 
-                                         ifelse(diff_predicted < 100 & diff_predicted > 0,
-                                                "<100",
-                                                ifelse(diff_predicted > -10 & diff_predicted < 0,
-                                                       ">-10",
-                                                       ifelse(diff_predicted > -100 & diff_predicted < 0,
-                                                              ">-100",
-                                                              .)
-                                                )))
-    ) %>%
-    mutate_at(.vars = c("col6"), ~ifelse(cumsum_diff_predicted < 10 & cumsum_diff_predicted > 0,
-                                         "<10", 
-                                         ifelse(cumsum_diff_predicted < 100 & cumsum_diff_predicted > 0,
-                                                "<100",
-                                                ifelse(cumsum_diff_predicted > -10 & cumsum_diff_predicted < 0,
-                                                       ">-10",
-                                                       ifelse(cumsum_diff_predicted > -100 & cumsum_diff_predicted < 0,
-                                                              ">-100",
-                                                              .)
-                                                )))
-    ) %>%
-    ## only keep the data for 1 month and 2 months post lockdown
-    filter(days2 == min(days2) | 
-             days3 == min(days3)) 
+    summarise(cumsum_ldn = sum(pred),
+           lci_cumsum_ldn = sum(low),
+           uci_cumsum_ldn = sum(upp),
+           cumsum_noLdn = sum(pred_noLdn),
+           lci_cumsum_noLdn = sum(low_noLdn),
+           uci_cumsum_noLdn = sum(upp_noLdn),
+           .groups = "keep"
+    ) %>% 
+    # estimated number of weekly outcomes with NO LOCKDOWN
+    mutate(cumsum_no_lockdown = paste0(prettyNum(cumsum_noLdn, big.mark=",",format = "f", digits = 3, scientific=FALSE),  # digits = 3 is sig. dig.
+                         " (", prettyNum(lci_cumsum_noLdn, big.mark=",",digits = 3, scientific=FALSE), 
+                         " - ", prettyNum(uci_cumsum_noLdn, big.mark=",",digits = 3, scientific=FALSE),")"),
+           # estimated number of weekly outcomes with LOCKDOWN
+           cumsum_with_lockdown = paste0(prettyNum(cumsum_ldn, big.mark=",",digits = 3, scientific=FALSE), 
+                         " (", prettyNum(lci_cumsum_ldn, big.mark=",",digits = 3, scientific=FALSE), 
+                         " - ", prettyNum(uci_cumsum_ldn, big.mark=",",digits = 3, scientific=FALSE),")")
+           )
   
-  rate_diff <- tab3_dates %>% 
-    mutate(rate_diff = (exp(pred)/denom) - (exp(pred_noLdn)/denom),
-           chisq_stat = (exp(pred) - (((exp(pred)+exp(pred_noLdn))*denom)/(denom+denom)))^2 / (((exp(pred)+exp(pred_noLdn))*denom*denom)/(denom^2)),
-           lci_rd = rate_diff - 1.96*(sqrt((rate_diff^2)/chisq_stat)),
-           uci_rd = rate_diff + 1.96*(sqrt((rate_diff^2)/chisq_stat))
-    ) %>%
-    select(rate_diff, lci_rd, uci_rd)
-  
-  
-  tab3_fmt <- tab3_dates %>% 
-    bind_cols(rate_diff) %>%
+  tab3_fmt <- tab3_cumsum %>% 
+    ungroup() %>% 
     mutate(outcome = outcome_of_interest_namematch$outcome_name[outcome_of_interest_namematch$outcome == outcome]) %>%
-    select(outcome, weekPlot, starts_with("col")) %>%
-    pivot_wider(values_from = starts_with("col")) %>%
-    mutate_at("weekPlot", ~as.character(format.Date(., "%d-%b"))) %>%
-    mutate_at("outcome", ~ifelse(row_number(.)==2, "", .))
+    select(outcome, imd, postcovid, cumsum_no_lockdown, cumsum_with_lockdown) %>%
+    mutate(postcovid = ifelse(postcovid == 0, "pre", "post")) %>% 
+    pivot_wider(id_cols = c(outcome, imd), names_from = postcovid, values_from = c(cumsum_no_lockdown, cumsum_with_lockdown)) %>%
+    mutate_at("outcome", ~ifelse(row_number(.)>1, "", .)) %>% 
+    # drop the cumsum_with_lockdown_pre: it is identical to cumsum_no_lockdown_post and it makes no sense to predict a lockdown pre covid
+    dplyr::select(-cumsum_with_lockdown_pre) 
+  
   return(tab3_fmt)
 }
 
@@ -169,5 +141,5 @@ for(ii in plot_order){
                     tab3_function(outcomes[ii]))
   tab3[nrow(tab3) + 1,] <- ""
 }
-tab3
+
 write.csv(tab3, file = here::here("./output/table3.csv"), row.names = F)
